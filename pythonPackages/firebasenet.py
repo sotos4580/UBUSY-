9e39dfa230f878b221c0859269828b54a8a28ce3
Evan <evansotos@gmail.com>
2018-12-11 16:50:41 -0600

    Update firebasenet.py

--- pythonPackages/firebasenet.py
+++ pythonPackages/firebasenet.py
@@ -19,12 +19,31 @@ firebase_admin.initialize_app(cred)
 db = firestore.client() #Init. firestore as admin
 #----- End Credential Init. -----#
 
-def pushData():
+def pushData(docCount):
     import netscanning
+    docName = "doc" + str(docCount)
 
     count = netscanning.count_active_hosts()
-    doc_ref = db.collection('postProcessed').document('collection')
+    doc_ref = db.collection('preProcessed').document(docName)
     doc_ref.set({
         'Time': current_min_time(),
         'People': count
     })
+
+def getCount():
+    peopleLists = []
+    timeLists = []
+    docs = ["doc0", "doc1", "doc2", "doc3", "doc4", "doc5", "doc6", "doc7", "doc8", "doc9"]
+    for i in range(10):
+        doc_ref = db.collection('preProcessed').document(docs[i])
+        try:
+            doc = doc_ref.get()
+            docData = (u'Document data: {}'.format(doc.to_dict()))
+            docData = docData.split()
+            for i in range(len(docData)):
+                if docData[i] == "{'People':":
+                    peopleLists.append(docData[i + 1])
+                if docData[i] == "'Time':":
+                    timeLists.append(docData[i + 1])
+        except google.cloud.exceptions.NotFound:
+            print(u'No such document!')
